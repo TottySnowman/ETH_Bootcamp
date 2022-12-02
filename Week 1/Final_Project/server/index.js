326cba879577d32e9daef49986a0b7680eaf602f
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2022-12-02 19:21:19 +0100

    Added validation of the transaction and funds transfer

--- Week 1/Final_Project/server/index.js
+++ Week 1/Final_Project/server/index.js
@@ -5,6 +5,9 @@ const port = 3042;
 const accounts = require("../server/constants/accounts");
 const hashing = require("./scripts/hashMessage");
 const sign = require("./scripts/signMessage");
+const recover = require("./scripts/recover");
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
 app.use(cors());
 app.use(express.json());
 
@@ -32,17 +35,27 @@ app.post("/send", async (req, res) => {
 
 let hash = hashing(JSON.stringify(transaction));
 let [signature, recoveryBit] = await sign(hash, privateKeySender);
-console.log(signature)
-  /* setInitialBalance(sender);
-  setInitialBalance(recipient);
+let recovered = await recover(JSON.stringify(transaction), signature, recoveryBit);
+let public_key = secp.getPublicKey(privateKeySender);
+let hexPublicKey = toHex(public_key);
 
+
+if(hexPublicKey == toHex(recovered))
+{
+  setInitialBalance(sender);
+  setInitialBalance(recipient);
   if (balances[sender] < amount) {
     res.status(400).send({ message: "Not enough funds!" });
   } else {
     balances[sender] -= amount;
     balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  } */
+    res.send({ balance: balances[sender]});
+  }
+}
+
+
+
+
 });
 
 app.listen(port, () => {
@@ -51,6 +64,7 @@ app.listen(port, () => {
 
 function setInitialBalance(address) {
   if (!balances[address]) {
+
     balances[address] = 0;
   }
 }
